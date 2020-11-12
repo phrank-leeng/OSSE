6de5a555f6af2e8699d531ae63492d2a3556e9e3
phrank-leeng <f.ling336@gmail.com>
2020-11-12 14:10:59 +0100

    added javadoc

--- src/main/java/de/hfu/Main.java
+++ src/main/java/de/hfu/Main.java
@@ -1,7 +1,18 @@
 package de.hfu;
 
+/**
+ * Main class
+ * 
+ * @author me
+ *
+ */
 public class Main {
 	
+	/**
+	 * gets input from console and prints it out in upper case
+	 * 
+	 * @param args the args
+	 */
 	public static void main(String[] args) {
 		System.out.println(System.console().readLine().toUpperCase());
 	}
